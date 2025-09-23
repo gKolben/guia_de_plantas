b8b33b37a96a0fc0e4a448078d75bb313f43eb0e
gKolben <gabrielakolbenpassos@gmail.com>
2025-09-23 15:24:13 -0300

    feat(ui): Implementa novo tema e paleta de cores

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,17 @@
+// Definição das cores principais do aplicativo
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._(); 
+
+  // Cores Principais
+  static const Color primaryGreen = Color(0xFF2FB201);
+  static const Color background = Color(0xFFFFFFFF);
+  
+  // Cores de Texto
+  static const Color textDark = Color(0xFF333333);
+  static const Color textLight = Color(0xFFFFFFFF);
+  
+  // Cores Neutras
+  static const Color grey = Color(0xFFCCCCCC);
+}
